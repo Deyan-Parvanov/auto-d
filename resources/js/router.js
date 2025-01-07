d8a6006f6b0a38d7f0984593af47f812609730cc
Deyan Parvanov <deyan.parvanov@delasport.com>
2025-01-07 18:31:36 +0200

    Refactoring - components names

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,17 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Index from './Pages/Listing/Index.vue';
-import Create from './Pages/CarDealer/Create.vue';
-import Edit from './Pages/CarDealer/Edit.vue';
-import Show from './Pages/Listing/Show.vue';
+import ListingIndex from './Pages/Listing/ListingIndex.vue';
+import CarDealerCreate from './Pages/CarDealer/CarDealerCreate.vue';
+import CarDealerEdit from './Pages/CarDealer/CarDealerEdit.vue';
+import ListingShow from './Pages/Listing/ListingShow.vue';
 import Login from './Pages/Auth/Login.vue';
 import Register from './Pages/UserAccount/Register.vue';
 import CarDealerIndex from './Pages/CarDealer/CarDealerIndex.vue';
 import CarDealerShow from './Pages/CarDealer/CarDealerShow.vue';
 import CreateImage from './Pages/CarDealer/ListingImage/CreateImage.vue';
 import NotificationIndex from './Pages/Notification/NotificationIndex.vue';
+import NotFound from './Pages/Error/NotFound.vue';
 import { useUserStore } from './stores/useUserStore';
 import { useListingsStore } from './stores/useListingsStore';
-import NotFound from './Pages/Error/NotFound.vue';
 
 // ToDo: move route guards to separate file and export it
 const requireAuthentication = (to, from, next) => {
@@ -71,15 +71,15 @@ const requireAuthorization = async (to, from, next) => {
 
 const routes = [
   { path: '/hello', name: 'hello' },
-  { path: '/listing', name: 'listing', component: Index },
-  { path: '/listing/:id', name: 'listingShow', component: Show },
+  { path: '/listing', name: 'listing', component: ListingIndex },
+  { path: '/listing/:id', name: 'listingShow', component: ListingShow },
   { path: '/login', name: 'login', component: Login },
   { path: '/logout', name: 'logout' },
   { path: '/register', name: 'register', component: Register },
   { path: '/car-dealer/listing', name: 'carDealerListing', component: CarDealerIndex },
   { path: '/car-dealer/listing/:id', name: 'carDealerListingShow', component: CarDealerShow, },
-  { path: '/car-dealer/listing/create', name: 'listingCreate', component: Create, beforeEnter: requireAuthentication, },
-  { path: '/car-dealer/listing/:id/edit', name: 'listingEdit', component: Edit, beforeEnter: requireAuthorization, },
+  { path: '/car-dealer/listing/create', name: 'listingCreate', component: CarDealerCreate, beforeEnter: requireAuthentication, },
+  { path: '/car-dealer/listing/:id/edit', name: 'listingEdit', component: CarDealerEdit, beforeEnter: requireAuthorization, },
   { path: '/car-dealer/listing/:id/image', name: 'listingImage', component: CreateImage, beforeEnter: requireAuthorization, },
   { path: '/notification', name: 'notification', component: NotificationIndex, beforeEnter: requireAuthentication, },
   { path: '/:catchAll(.*)', name: 'NotFound', component: NotFound},
